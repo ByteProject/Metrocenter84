59f4c29343cd27ab2bcb2dd80410429b98a98d3b
ByteProject <ByteProject@users.noreply.github.com>
2020-06-28 12:36:23 +0200

    adding in-header documentation

--- metro84/scenery.h
+++ metro84/scenery.h
@@ -3,6 +3,60 @@
 !  (c) Fredrik Ramsberg (author) 2020.
 !  (c) Stefan Vogt, Puddle Software 2020.
 ! ----------------------------------------------------------------------------
+!
+! This library extension provides a way to implement simple scenery objects 
+! which can only be examined, using just a single object for the entire game.
+! This helps keep both the object count and the dynamic memory usage down.
+!
+! To use it, include this file after globals.h. Then add a property called 
+! cheap_scenery to the locations where you want to add cheap scenery objects.
+! You can add up to ten cheap scenery objects to one location in this way. For 
+! each scenery object, specify, in this order, one adjective, one noun, and one
+! description string or a routine to print one. Instead of an adjective, you
+! may give a synonym to the noun. If no adjective or synonym is needed, 
+! use the value 1 in that position.
+! 
+! Note: If you want to use this library extension is a Z-code version 3 game, 
+! you must NOT declare cheap_scenery as a common property, or it will only be 
+! able to hold one scenery object instead of ten.
+!
+! If you want to use the same description for a scenery object in several locations,
+! declare a constant to hold that string, and refer to the constant in each location.
+!
+! Before including this extension, you can also define a string or routine called 
+! SceneryReply. If you do, it will be used whenever the player does something to a 
+! scenery object other than examining it. If it's a string, it's printed. If it's a
+! routine it's called. If the routine prints something, it should return true, 
+! otherwise false. 
+!
+! Example usage:
+
+! [SceneryReply;
+!   Push:
+!     "Now how would you do that?";
+!   default:
+!     rfalse;
+! ];
+!
+! Include "ext_cheap_scenery.h";
+!
+! Constant SCN_WATER = "The water is so beautiful this time of year, all clear and glittering.";
+! [SCN_SUN; 
+!   deadflag = 1;
+!   "As you stare right into the sun, you feel a burning sensation in your eyes. 
+!     After a while, all goes black. With no eyesight, you have little hope of
+!     completing your investigations."; 
+! ];
+!
+! Object RiverBank "River Bank"
+!   with
+!	 description "The river is quite wide here. The sun reflects in the blue water, the birds are 
+!      flying high up above.",
+!	 cheap_scenery
+!      'blue' 'water' SCN_WATER
+!      'bird' 'birds' "They seem so careless."
+!      1 'sun' SCN_SUN,
+!   has light;
 
 System_file;
 
